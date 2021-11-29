43929364cc2a9cd3be93b01c3b88030b4774ad63
juancarlosMJ <mendez_30_yo@hotmail.com>
2021-11-29 10:46:32 -0600

    co

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { iif } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-model';
+  title = 'Usando modelos en angular ';
+  public inventario :any=[
+    {id : 1, nombre : "pantalon",precio : 250},
+    {id : 2, nombre : "chamarra",precio : 500},
+  ];
+  public formulario : any = {
+    id:null,
+    nombre : null,
+    precio : null
+  }
+  public seleccionar (articulos : any) : void{
+    this.formulario.id = articulos.id;
+    this.formulario.nombre = articulos.nombre;
+    this.formulario.precio = articulos.precio;
+
+  }
+  public eliminar (id : number):void{
+    for (let index = 0; index < this.inventario.length; index++) {
+      if (this.inventario[index].id == id) {
+        this.inventario.splice(index,1);
+        alert("eliminado con exito!");
+        break;
+        
+      }      
+    }
+
+  }
+  public agregar() : void{
+    let datoNuevo = {
+      id : this.formulario.id,
+      nombre : this.formulario.nombre,
+      precio : this.formulario.precio
+    };
+    this.inventario.push(datoNuevo);
+    alert("agregado con exito");
+    
+  }
+  public editar() : void{
+    for (let index = 0; index < this.inventario.length; index++) {
+      if (this.inventario[index].id == this.formulario.id) {
+        this.inventario[index].id = this.formulario.id;
+        this.inventario[index].nombre = this.formulario.nombre;
+        this.inventario[index].precio = this.formulario.precio;
+        alert("Modificado con exito");
+        break;
+        
+      }
+      
+    }
+
+  }
 }
